37ad6788ab1dc941e0840966fd8a1f3b0ee05cf9
William Jamieson <wjamieson@stsci.edu>
2023-02-09 13:19:26 -0500

    Create the script which the package installs

--- src/minimum_dependencies/__init__.py
+++ src/minimum_dependencies/__init__.py
@@ -1,5 +1,5 @@
 """Generate a minimum dependencies file for a Python project."""
-from ._core import write
+from ._core import create, write
 from ._version import version as __version__
 
-__all__ = ["write", "__version__"]
+__all__ = ["create", "write", "__version__"]
---
+++ src/minimum_dependencies/_script.py
@@ -0,0 +1,45 @@
+"""Contain the function linked to by the entry point."""
+
+
+from argparse import ArgumentParser
+from itertools import chain
+
+from ._core import write
+
+
+def argparser() -> ArgumentParser:
+    """Create the argument parser."""
+    parser = ArgumentParser(
+        "minimum_deps",
+        description="Generate a requirements-min.txt file based on install_requires",
+    )
+    parser.add_argument(
+        "package",
+        type=str,
+        nargs=1,
+        help="Name of the package to generate requirements for",
+    )
+    parser.add_argument(
+        "--filename",
+        "-f",
+        default=None,
+        help="Name of the file to write out",
+    )
+    parser.add_argument(
+        "--extras",
+        "-e",
+        nargs="*",
+        default=None,
+        action="append",
+        help="List of optional dependency sets to include",
+    )
+    return parser
+
+
+def main() -> None:
+    """Run the script."""
+    parser = argparser()
+    args = parser.parse_args()
+    extras = None if args.extras is None else list(chain.from_iterable(args.extras))
+
+    write(args.package[0], args.filename, extras)
