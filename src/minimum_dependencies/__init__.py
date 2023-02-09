52c57e5785ed0a81b4c9759d23d31e3da0c48064
William Jamieson <wjamieson@stsci.edu>
2023-02-09 00:51:33 -0500

    Add core functionality

--- src/minimum_dependencies/__init__.py
+++ src/minimum_dependencies/__init__.py
@@ -1,4 +1,5 @@
 """Generate a minimum dependencies file for a Python project."""
+from ._core import write
 from ._version import version as __version__
 
-__all__ = ["__version__"]
+__all__ = ["write", "__version__"]
---
+++ src/minimum_dependencies/_core.py
@@ -0,0 +1,82 @@
+"""Core functionality for minimum_dependencies."""
+
+import warnings
+from contextlib import suppress
+from pathlib import Path
+from sys import stdout
+
+import requests
+from importlib_metadata import requires
+from packaging.requirements import Requirement
+from packaging.version import InvalidVersion, Version, parse
+
+
+def versions(requirement: Requirement) -> list(Version):
+    """Get the versions available on PyPi for a given requirement."""
+    content = requests.get(
+        f"https://pypi.python.org/pypi/{requirement.name}/json",
+        timeout=30,
+    ).json()
+
+    versions = []
+    for v in content["releases"]:
+        with suppress(InvalidVersion):
+            versions.append(parse(v))
+
+    return sorted(versions)
+
+
+def minimum_version(requirement: Requirement) -> Version:
+    """Return minimum version available on PyPi for a given version specification."""
+    if not requirement.specifier:
+        warnings.warn(
+            f"No version specifier for {requirement.name} in "
+            "install_requires.  Using lowest available version on PyPi.",
+            stacklevel=2,
+        )
+
+    for version in versions(requirement):
+        if version in requirement.specifier:
+            # If the requirement does not list any version, the lowest will be
+            return version
+
+    # If the specified version does not exist on PyPi, issue a warning
+    # and return the lowest available version
+    warnings.warn(
+        f"Exact version specified in {requirement} not found on PyPi. "
+        "Using lowest available version.",
+        stacklevel=2,
+    )
+    return versions[0]
+
+
+def create(package: str, extras: list = None) -> str:
+    """Create a list of requirements for a given package."""
+    extras = [] if extras is None else extras
+
+    requirements = []
+    for r in requires(package):
+        requirement = Requirement(r)
+
+        if requirement.marker is None or any(
+            requirement.marker.evaluate({"extra": e}) for e in extras
+        ):
+            if requirement.url is None:
+                requirements.append(
+                    f"{requirement.name}=={minimum_version(requirement)}\n",
+                )
+            else:
+                requirements.append(f"{requirement}\n")
+
+    return "".join(requirements)
+
+
+def write(package: str, filename: str, extras: list = None) -> None:
+    """Write out a requirements file for a given package."""
+    requirements = create(package, extras=extras)
+
+    if filename is None:
+        stdout.write(requirements)
+    else:
+        with Path(filename).open("w") as fd:
+            fd.write(requirements)
